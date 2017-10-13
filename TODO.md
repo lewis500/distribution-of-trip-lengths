08e9f431cd1e8250cc9c928c31a4492cf3242c4c
Lewis Lehe <lewis500@gmail.com>
2017-10-13 13:52:40 -0700

    new version

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
-+ remove the dumb float specfications
 + write a markdown
-+ rename the images in the latex files
++ surplus is veh-min/min/lane-km
--- simulations.jl
+++ simulations.jl
@@ -21,7 +21,9 @@ struct World
   λ::Float64
   pf::Float64
   k0::Float64
-  z::MultivariateDistribution
+  μ::Array{Float64}
+  Σ::Array{Float64}
+  z::MultivariateDistribution #this is produced from Σ and μ
 end
 #the generator function for World objects. All it does is take the means and covariance matrix used to produce the population, and creates a bivariate lognormal distribution for it as the z parameter in the World object
 function World(;
@@ -31,7 +33,7 @@ function World(;
   μ::Array{Float64} = [1.0; 1.0],
   Σ::Array{Float64} = [.1 .1; .1 .1]
   )
-  return World(λ,pf,k0,MvLogNormal(μ, Σ))
+  return World(λ,pf,k0,μ,Σ,MvLogNormal(μ, Σ))
 end
 
 # Equilibrium is defined as type consisting of a density, a pace and a circulation
@@ -42,26 +44,26 @@ pk(w::World, k::Float64)::Float64 = w.pf*exp(.5(k/w.k0)^2)
 qk(w::World, k::Float64)::Float64 = k/pk(w,k)
 
 # X() is as defined as in the paper: the disutility of a car trip when pace is p, trip length is l and the given tolls
-X(p::Float64,l::Float64;τt::Float64=0.,τa::Float64=0.)::Float64 = (p+τa)*l + τt
+X(p::Float64,l::Float64;τa::Float64=0.,τd::Float64=0.)::Float64 = (p+τd)*l + τa
 
 ##### the following block gives all the functions required to calculate the equilbrium.
 ##### each takes a "World" (a set of parameters), a pace p and the tolls given by obvious keyword arguments
 
 # qdp gives the cirulation depanded for a given p and set of tolls.
-function qdp(w::World,p::Float64;τt::Float64=0.,τa::Float64=0.)
+function qdp(w::World,p::Float64;τa::Float64=0.,τd::Float64=0.)
   function f(x)::Float64
     l::Float64 = x[2]/(1-x[2])
-    ɛ::Float64 = (p+τa)*l + τt + x[1]/(1-x[1])
+    ɛ::Float64 = (p+τd)*l + τa + x[1]/(1-x[1])
     return l*pdf(w.z,[ɛ;l])/((1-x[2])*(1-x[1]))^2
   end
   return w.λ*hcubature(f,[0.,0.],[1.,1.];abstol=1e-4)[1]
 end
 
 # adp gives the arrival flow demanded for a given p and set of tolls
-function adp(w::World,p::Float64;τt::Float64=0.,τa::Float64=0.)::Float64
+function adp(w::World,p::Float64;τa::Float64=0.,τd::Float64=0.)::Float64
   function f(x)::Float64
       l::Float64 = x[2]/(1-x[2])
-      cost::Float64 = (p+τa)*l + τt
+      cost::Float64 = (p+τd)*l + τa
       ɛ::Float64 = cost+ x[1]/(1-x[1])
       pdf(w.z,[ɛ;l])/((1-x[2])*(1-x[1]))^2
   end
@@ -69,10 +71,10 @@ function adp(w::World,p::Float64;τt::Float64=0.,τa::Float64=0.)::Float64
 end
 
 # TSS gives the total social surplus for a given p and set of tolls
-function TSS(w::World,p::Float64;τt::Float64=0.,τa::Float64=0.)::Float64
+function TSS(w::World,p::Float64;τa::Float64=0.,τd::Float64=0.)::Float64
   function f(x)::Float64
     l::Float64 = x[2]/(1-x[2])
-    cost::Float64 = (p+τa)*l + τt
+    cost::Float64 = (p+τd)*l + τa
     ɛ::Float64 = cost + x[1]/(1-x[1])
     pdf(w.z,[ɛ;l])*(ɛ-l*p)/((1-x[2])*(1-x[1]))^2
   end
@@ -80,10 +82,10 @@ function TSS(w::World,p::Float64;τt::Float64=0.,τa::Float64=0.)::Float64
 end
 
 # CS gives the consumer surplus for a given p and set of tolls
-function CS(w::World,p::Float64;τt::Float64=0.,τa::Float64=0.)::Float64
+function CS(w::World,p::Float64;τa::Float64=0.,τd::Float64=0.)::Float64
   function f(x)::Float64
     l::Float64 = x[2]/(1-x[2])
-    cost::Float64 = (p+τa)*l + τt
+    cost::Float64 = (p+τd)*l + τa
     ɛ::Float64 = cost + x[1]/(1-x[1])
     pdf(w.z,[ɛ;l])*(ɛ-cost)/((1-x[2])*(1-x[1]))^2
   end
@@ -92,24 +94,24 @@ end
 
 #### the functions chooseA and chooseT choose the welfare-maximizing access and trip toll levels
 function chooseA(w::World)::Float64
-  optimize(τa::Float64->begin
-    p= getEq(w;τa=τa)[2]
-    -TSS(w,p;τa=τa)
+  optimize(τd::Float64->begin
+    p= getEq(w;τd=τd)[2]
+    -TSS(w,p;τd=τd)
   end,0.,4.0,
   ;abs_tol=1e-4)|>minimizer
 end
 
 function chooseT(w::World)::Float64
-  optimize(τt::Float64->begin
-    -TSS(w,getEq(w;τt=τt)[2];τt=τt)
+  optimize(τa::Float64->begin
+    -TSS(w,getEq(w;τa=τa)[2];τa=τa)
   end,1.,10.,
   ;abs_tol=1e-4)|>minimizer
 end
 
 ##### the getEq function finds the equilibrium given tolls for a given "world"
-function getEq(w::World;τt::Float64=0.,τa::Float64=0.)::Equilibrium
+function getEq(w::World;τa::Float64=0.,τd::Float64=0.)::Equilibrium
   ke::Float64 = fzero(
-    k::Float64-> qk(w,k) - qdp(w,pk(w,k); τt=τt,τa=τa)
+    k::Float64-> qk(w,k) - qdp(w,pk(w,k); τa=τa,τd=τd)
     ,w.k0;order=2)
   return (ke, pk(w,ke), qk(w,ke))
 end
@@ -122,12 +124,16 @@ function plotSupplyAndDemand(w::World)
   k,p,q = getEq(w)
   close("all")
   fig,ax = subplots(figsize=(4,3))
-  ax[:plot](qsRange,pRange,label=L"p_s/\tilde{p}_s", ls="-")
-  ax[:plot](map(p->qdp(w,p),pRange),pRange,label=L"p_d(q)",ls="-.")
-  τa = chooseA(w)
-  ax[:plot](map(p->qdp(w,p;τa=τa,τt=0.),pRange),pRange,label=L"p_d(q;\tau_d^*)",ls=":")
-  τt = chooseT(w)
-  ax[:plot](map(p->qdp(w,p;τt=τt,τa=0.),pRange),pRange,label=L"p_d(q;\tau_a^*)",ls="--")
+  # supply curve
+  ax[:plot](qsRange,pRange,label=L"p_s/\tilde{p}_s", ls="-",color="royalblue")
+  # untolled
+  ax[:plot](map(p->qdp(w,p),pRange),pRange,label=L"p_d(q;0,0)",ls="-.",color="coral")
+  # distance toll
+  τd = chooseA(w)
+  ax[:plot](map(p->qdp(w,p;τd=τd,τa=0.),pRange),pRange,label=L"p_d(q;\tau_d^*,0)",ls=":",color="green")
+  # access toll
+  τa = chooseT(w)
+  ax[:plot](map(p->qdp(w,p;τa=τa,τd=0.),pRange),pRange,label=L"p_d(q;0,\tau_a^*)",ls="--",color="red")
   ax[:set_xlim]((0,qk(w,w.k0)*1.2))
   ax[:set_ylim]((0,pRange[end]))
   ax[:set_xlabel](L"q")
@@ -141,30 +147,30 @@ end
 ##### plotTripLengths takes a "World," find the optimal access and distance tolls and then plots the trip length distribution
 function plotTripLengths(w)
   k,p,q = getEq(w)
-  τa = chooseA(w)
-  kd,pd,qd = getEq(w;τa=τa)
-  τt = chooseT(w)
-  kt,pt,qt = getEq(w;τt = τt)
+  τd = chooseA(w)
+  kd,pd,qd = getEq(w;τd=τd)
+  τa = chooseT(w)
+  kt,pt,qt = getEq(w;τa = τa)
 
   lRange = linspace(0,mean(w.z)[2]*2,100)
 
-  function plotEq(w::World,ax;label="o",ls="-",τt=0.,τa=0.)
-    k,p,q = getEq(w;τt=τt,τa=τa)
+  function plotEq(w::World,ax;label="o",ls="-",τa=0.,τd=0.,color="blue")
+    k,p,q = getEq(w;τa=τa,τd=τd)
     mass = map(l->begin
       quadgk(x-> begin
-        ɛ = X(p,l;τt=τt,τa=τa) + x/(1-x)
+        ɛ = X(p,l;τa=τa,τd=τd) + x/(1-x)
         pdf(w.z,[ɛ;l])
       end,0.,1.)[1]
     end,lRange)
-    ax[:plot](lRange,mass,label=label,ls=ls)
+    ax[:plot](lRange,mass,label=label,ls=ls,color=color)
   end
 
   fig,ax = subplots(figsize=(4,3))
   ax[:set_xlabel]("km")
   ax[:set_ylabel]("density")
-  plotEq(w,ax;label=L"g(l;untolled)",ls="-")
-  plotEq(w,ax;label=L"g(l;\tau_a^*)",ls=":",τt=τt)
-  plotEq(w,ax;label=L"g(l;\tau_d^*)",ls="-.",τa=τa)
+  plotEq(w,ax;label=L"g(l;0,0)",ls="-.",color="coral")
+  plotEq(w,ax;label=L"g(l;\tau_d^*,0)",τd=τd,ls=":",color="green")
+  plotEq(w,ax;label=L"g(l;0,\tau_a^*)",τa=τa,ls="--",color="red")
   ax[:legend](frameon=false)
   fig[:tight_layout]()
   fig
@@ -179,19 +185,19 @@ function createTable(w::World)
   cs = CS(w,p)
   tr = tss - cs
 
-  τa = chooseA(w)
-  kd,pd,qd = getEq(w;τa=τa)
-  ad = adp(w,pd;τa=τa)
-  tssd = TSS(w,pd;τa=τa)
-  csd = CS(w,pd;τa=τa)
+  τd = chooseA(w)
+  kd,pd,qd = getEq(w;τd=τd)
+  ad = adp(w,pd;τd=τd)
+  tssd = TSS(w,pd;τd=τd)
+  csd = CS(w,pd;τd=τd)
   ld = qd/ad
   trd = tssd - csd
 
-  τt = chooseT(w)
-  kt,pt,qt = getEq(w;τt = τt)
-  at = adp(w,pt;τt=τt)
-  tsst = TSS(w,pt;τt=τt)
-  cst = CS(w,pt;τt=τt)
+  τa = chooseT(w)
+  kt,pt,qt = getEq(w;τa = τa)
+  at = adp(w,pt;τa=τa)
+  tsst = TSS(w,pt;τa=τa)
+  cst = CS(w,pt;τa=τa)
   lt = qt/at
   trt = tsst - cst
 
@@ -199,7 +205,7 @@ function createTable(w::World)
   rounder(arr::Array{Float64})::Array{Float64} = map(x-> round(x,1) , arr)
   return DataFrame(
     toll=["none","distance","access"],
-    τ=[0,τa,τt]|>rounder,
+    τ=[0,τd,τa]|>rounder,
     k=[k,kd,kt]|>rounder,
     l=[l,ld,lt]|>rounder,
     p=[p,pd,pt]|>rounder,
@@ -225,13 +231,13 @@ function plotDivision(w::World)
   fig,ax = subplots(figsize=(4,3))
   ax[:contourf](X,Y,Z,cmap=:Blues)
   k,p,q = getEq(w)
-  ax[:plot](X, map(l->l*p, X),label=L"D_{untolled}",ls="-",c="k",lw=2)
-  τa = chooseA(w)
-  kd,pd,qd = getEq(w;τa=τa)
-  ax[:plot](X, map(l->l*(pd + τa), X),label=L"D_{\tau_d^*}",ls=":",c="m",lw=2)
-  τt = chooseT(w)
-  kt,pt,qt = getEq(w;τt = τt)
-  ax[:plot](X, map(l->l*pt + τt, X),label=L"D_{\tau_a^*}",ls="--",c="r",lw=2)
+  ax[:plot](X, map(l->l*p, X),label=L"D_{untolled}",ls="-.",c="coral",lw=2)
+  τd = chooseA(w)
+  kd,pd,qd = getEq(w;τd=τd)
+  ax[:plot](X, map(l->l*(pd + τd), X),label=L"D_{\tau_d^*}",ls=":",c="g",lw=2)
+  τa = chooseT(w)
+  kt,pt,qt = getEq(w;τa = τa)
+  ax[:plot](X, map(l->l*pt + τa, X),label=L"D_{\tau_a^*}",ls="--",c="r",lw=2)
   ax[:set_xlim](0,xMax)
   ax[:set_ylim](0,yMax)
   ax[:legend]()
@@ -245,17 +251,20 @@ function plotDivision(w::World)
 end
 
 #### plotAdvantage creates the figure showing the advantage of the distance toll over the access toll for various values of covariance between benefit and trip length
-function plotAdvantage()
+function plotAdvantage(w::World)
   n = 20
   cvRange = linspace(-.15,.15,n)
   res = map(cv->begin
-    w = World(;μ=[2.4,1.0],Σ=[0.2 cv; cv 0.2], λ=20.,pf=2.2,k0=55.)
-    τa = chooseA(w)
-    kd,pd,qd = getEq(w;τa=τa)
-    tssd = TSS(w,pd;τa=τa)
-    τt = chooseT(w)
-    kt,pt,qt = getEq(w;τt=τt)
-    tsst = TSS(w,pt;τt=τt)
+    w = World(;
+      μ=w.μ,
+      Σ=[w.Σ[1,1] cv; cv w.Σ[2,2]],
+      λ=w.λ,pf=w.pf,k0=w.k0)
+    τd = chooseA(w)
+    kd,pd,qd = getEq(w;τd=τd)
+    tssd = TSS(w,pd;τd=τd)
+    τa = chooseT(w)
+    kt,pt,qt = getEq(w;τa=τa)
+    tsst = TSS(w,pt;τa=τa)
     tssd/tsst-1
   end,cvRange)
   fig,ax = subplots(figsize=(4,3))
@@ -269,19 +278,26 @@ end
 end
 ###### in this part of the code you define the free parameters and run whichever functions you want for the plot and table, saving them to disk in the images folder
 
-#here, w is a variable holding the parameters used in the paper. you can modify it with your own parameters.
-w = Simulation.World(;μ=[2.4,1.0],Σ=[0.2 0.12; 0.12 0.2], λ=20.,pf=2.2,k0=55.)
+#here, world is a "World" variable holding the parameters used in the paper. you can modify it with your own parameters.
+world = Simulation.World(;
+  μ=[2.4,1.0],
+  Σ=[0.2 0.12; 0.12 0.2],
+  λ=20.,
+  pf=2.2,
+  k0=55.)
 
-Simulation.createTable(w)
+# print the table
+Simulation.createTable(world)
 
-fig = Simulation.plotTripLengths(w)
+# make the plots
+fig = Simulation.plotTripLengths(world)
 fig[:savefig]("img/tripLengths.pdf")
 
-fig2 = Simulation.plotAdvantage()
-fig2[:savefig]("img/advantage.pdf")
+# fig2 = Simulation.plotAdvantage(world)
+# fig2[:savefig]("img/advantage.pdf")
 
-fig3 = Simulation.plotSupplyAndDemand(w)
+fig3 = Simulation.plotSupplyAndDemand(world)
 fig3[:savefig]("img/supplyDemand.pdf")
 
-fig4 = Simulation.plotDivision(w)
+fig4 = Simulation.plotDivision(world)
 fig4[:savefig]("img/division.pdf")
